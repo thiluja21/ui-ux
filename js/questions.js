dbc85d20a3571c3f4ec730001e20b7df91a0daeb
Thiluja krishnamoorthy <thiluja@vetstoria.com>
2022-01-24 12:09:40 +0530

    ui/ux changes makes

---
+++ js/questions.js
@@ -0,0 +1,64 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Name a vegetable that is rich in vitamin A?",
+    answer: "sweet potato",
+    options: [
+      "sweet potato",
+      "Beetroot"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the largest vegetable ?",
+    answer: "Pumpkin",
+    options: [
+      "Pumpkin",
+      "Cabbage"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Honey is made up of bee vomit? ",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "What type of food is basmathi?",
+    answer: "long-grain",
+    options: [
+      "Jasmine rice",
+      "long-grain"
+    ]
+  },
+    {
+    numb: 5,
+    question: "which fish is the main ingredient of Scotch Woodcock?",
+    answer: "salmon",
+    options: [
+      "savoury",
+      "salmon"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
